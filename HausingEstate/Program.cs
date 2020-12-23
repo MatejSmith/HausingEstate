e7c6b642eb7c620bd7d605f9a68a229ed3a6f459
MatejSmith <mtjkovac@gmail.com>
2020-12-23 19:44:29 +0100

    fix name

--- HausingEstate/Program.cs
+++ HausingEstate/Program.cs
@@ -8,7 +8,7 @@ namespace HousingEstate
         static void Main(string[] args)
         {
             Flat piestanska = new Flat(24, 100, 4);
-            piestanska.AddHabitant("matej", "surname", 17);
+            piestanska.AddHabitant("Matej", "Kovac", 17);
 
             piestanska.HabitantsInfo();
             Console.WriteLine(piestanska.ToString());
